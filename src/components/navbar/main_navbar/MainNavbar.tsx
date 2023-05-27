b8a238c05ba12f2367aa3e1779ea3aea61fafa38
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-05-28 04:52:34 +0630

    feat:responsive for mobile and tablet

--- src/components/navbar/main_navbar/MainNavbar.tsx
+++ src/components/navbar/main_navbar/MainNavbar.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import styles from "./_mainNavbar.module.scss";
 import { mainNavbarConstant } from "@/constants/mainNavbar/mainNavbar";
+import { IconBrandApple, IconBriefcase, IconSearch } from "@tabler/icons-react";
 
 const MainNavbar = () => {
   return (
     <div className={styles.body_main_nav}>
       <div>
-        {mainNavbarConstant.map((c) => {
-          const key = typeof c === "string" ? c : c.key;
-          const element = typeof c === "string" ? c : c.element;
+        <IconBrandApple />
+        {mainNavbarConstant.map((c, index) => {
           return (
-            <div className={styles.poi} key={key}>
-              {element}
+            <div className={styles.poi} key={index}>
+              {c}
             </div>
           );
         })}
+        <section
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "20px",
+            alignItems: "center",
+          }}
+        >
+          <IconSearch size='18px' />
+          <IconBriefcase size='18px' />
+        </section>
       </div>
     </div>
   );
--- src/constants/mainNavbar/mainNavbar.tsx
+++ src/constants/mainNavbar/mainNavbar.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { IconBrandApple, IconBriefcase, IconSearch } from "@tabler/icons-react";
 
 export const mainNavbarConstant = [
-  { key: "icon-apple", element: <IconBrandApple /> },
   "Store",
   "Mac",
   "iPad",
@@ -13,6 +12,4 @@ export const mainNavbarConstant = [
   "Entertainment",
   "Accessories",
   "Support",
-  { key: "icon-search", element: <IconSearch size='18px' /> },
-  { key: "icon-briefcase", element: <IconBriefcase size='18px' /> },
 ];
